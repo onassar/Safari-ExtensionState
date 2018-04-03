a7964d90cfe414093289546a5ac466d722fba7d2
Oliver Nassar <onassar@gmail.com>
2018-04-04 00:28:30 +0100

    Create SafariExtensionState.js

---
+++ SafariExtensionState.js
@@ -0,0 +1,101 @@
+
+/**
+ * ExtensionState
+ * 
+ * @abstract
+ */
+var ExtensionState = (function() {
+
+    /**
+     * __configFilePath
+     * 
+     * @access  private
+     * @return  String (default: '../Info.plist')
+     */
+    var __configFilePath = '../Info.plist';
+
+    /**
+     * __getConfigVersion
+     * 
+     * @access  private
+     * @param   Function callback
+     * @return  void
+     */
+    var __getConfigVersion = function(callback) {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open('get', __configFilePath);
+        xmlhttp.onload = function () {
+            var infoFile = xmlhttp.responseXML,
+                keys = infoFile.getElementsByTagName('key');
+            for (var i = 0; i < keys.length; i++){
+                if (keys[i].firstChild.data === 'CFBundleShortVersionString') {
+                    var version = keys[i].nextElementSibling.firstChild.data;
+                    callback(version);
+                    break;
+                }
+            }
+        };
+        xmlhttp.send();
+    };
+
+    /**
+     * __getLocalVersion
+     * 
+     * @access  private
+     * @return  String
+     */
+    var __getLocalVersion = function() {
+        return localStorage['version'];
+    };
+
+    /**
+     * __putLocalVersion
+     * 
+     * @access  private
+     * @param   String version
+     * @return  void
+     */
+    var __putLocalVersion = function(version) {
+        localStorage['version'] = version;
+    };
+
+    // Public
+    return {
+
+        /**
+         * installed
+         * 
+         * @access  public
+         * @param   Function callback
+         * @return  void
+         */
+        installed: function(callback) {
+            var localVersion = __getLocalVersion();
+            if (typeof localVersion === 'undefined') {
+                __getConfigVersion(function(version) {
+                    callback(version);
+                    __putLocalVersion(version);
+                });
+            }
+        },
+
+        /**
+         * updated
+         * 
+         * @access  public
+         * @param   Function callback
+         * @return  void
+         */
+        updated: function(callback) {
+            var localVersion = __getLocalVersion();
+            if (typeof localVersion !== 'undefined') {
+                __getConfigVersion(function(version) {
+                    if (localVersion !== version) {
+                        callback(version);
+                        __putLocalVersion(version);
+                    }
+                });
+            }
+        }
+    };
+})();
